cf31fbd1d017bae92bc219e47d8f478fda9020d0
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-01-11 12:08:06 +0530

    add: keyevents component component to sentiment

---
+++ src/components/Main/Sentiment/KeyEvents.jsx
@@ -0,0 +1,47 @@
+import InfoSVG from '../../../assets/InfoSVG';
+import NotesSVG from '../../../assets/NotesSVG';
+import AnalyticsSVG from '../../../assets/AnalyticsSVG';
+
+const KeyEvents = () => {
+  return (
+    <>
+      <h2 className='flex items-center text-xl font-semibold text-gray-700 mt-4'>
+        Key Events <InfoSVG />
+      </h2>
+      <div className='overflow-x-scroll mt-3 w-auto flex gap-2'>
+        <div className='bg-blue-50 p-4 rounded-md flex gap-2 min-w-[345px]'>
+          <div className='pt-2'>
+            <NotesSVG />
+          </div>
+          <div>
+            <p className='font-semibold leading-tight text-sm pb-2'>
+              Lorem ipsum dolor sit amet consectetur. Dui vel quis dignissim mattis enim{' '}
+            </p>
+            <p className='text-xs text-gray-600'>
+              Lorem ipsum dolor sit amet consectetur. Ac phasellus risus est faucibus metus quis.
+              Amet sapien quam viverra adipiscing condimentum. Ac consectetur et pretium in a
+              bibendum in. Sed vitae sit nisi viverra natoque lacinia libero enim.
+            </p>
+          </div>
+        </div>
+        <div className='bg-green-50 p-4 rounded-md flex gap-2 min-w-[345px]'>
+          <div className='pt-2'>
+            <AnalyticsSVG />
+          </div>
+          <div>
+            <p className='font-semibold leading-tight text-sm pb-2'>
+              Lorem ipsum dolor sit amet consectetur. Dui vel quis dignissim mattis enim{' '}
+            </p>
+            <p className='text-xs text-gray-600'>
+              Lorem ipsum dolor sit amet consectetur. Ac phasellus risus est faucibus metus quis.
+              Amet sapien quam viverra adipiscing condimentum. Ac consectetur et pretium in a
+              bibendum in. Sed vitae sit nisi viverra natoque lacinia libero enim.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default KeyEvents;
--- src/components/Main/Sentiment/Sentiment.jsx
+++ src/components/Main/Sentiment/Sentiment.jsx
@@ -1,7 +1,9 @@
+import KeyEvents from "./KeyEvents";
 const Sentiment = () => {
   return (
     <div className='flex flex-col bg-white rounded-md mx-auto p-7 w-full mt-5'>
       <h3 className='text-2xl font-semibold'>Sentiment</h3>
+      <KeyEvents />
     </div>
   );
 };
