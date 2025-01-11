4792cd0a10f8ed06c0c1b5c9af4875a942ce60cb
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-01-11 11:58:05 +0530

    add: assests and images

---
+++ src/assets/ArrowSVG.jsx
@@ -0,0 +1,22 @@
+const ArrowSVG = () => {
+  return (
+    <svg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M16.3036 10.0633H4.64001'
+        stroke='#0F1629'
+        strokeWidth='1.39963'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M11.5995 5.37897L16.3038 10.0631L11.5995 14.7479'
+        stroke='#0F1629'
+        strokeWidth='1.39963'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default ArrowSVG;
---
+++ src/assets/InfoSVG.jsx
@@ -0,0 +1,19 @@
+const InfoSVG = () => {
+  return (
+    <svg width='27' height='20' viewBox='0 0 27 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <g clipPath='url(#clip0_57_680)'>
+        <path
+          d='M16.04 1.66663C11.44 1.66663 7.70667 5.39996 7.70667 9.99996C7.70667 14.6 11.44 18.3333 16.04 18.3333C20.64 18.3333 24.3733 14.6 24.3733 9.99996C24.3733 5.39996 20.64 1.66663 16.04 1.66663ZM16.8733 14.1666H15.2067V9.16663H16.8733V14.1666ZM16.8733 7.49996H15.2067V5.83329H16.8733V7.49996Z'
+          fill='#ABB9BF'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_57_680'>
+          <rect width='20' height='20' fill='white' transform='translate(6.03998)' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default InfoSVG;
