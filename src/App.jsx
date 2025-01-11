fbc75acb5bb9c76ee3416a6db697c582e090bfd7
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-01-11 12:30:12 +0530

    remove: footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Main />
-      <Footer />
     </>
   );
 }
