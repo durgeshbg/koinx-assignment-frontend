6dfd76616e219fed3e00400ef6b2a5fd4469f728
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-01-11 21:38:29 +0530

    feat: Trending data values fetched

--- src/components/Main/TrendingCoins/TrendingCoins.jsx
+++ src/components/Main/TrendingCoins/TrendingCoins.jsx
@@ -4,15 +4,29 @@ import { trendingCoinsAtom } from '../../../Atoms';
 
 const TrendingCoins = () => {
   const data = useRecoilValue(trendingCoinsAtom);
-  console.log(data.coins);
-  
+
   return (
     <div className='flex flex-col bg-white rounded-sm p-7 mt-5 lg:mx-5 lg:rounded-md'>
       <h3 className='font-semibold text-xl'>Trending Coins (24H)</h3>
       <ul>
-        <TrendingItem />
-        <TrendingItem />
-        <TrendingItem />
+        <TrendingItem
+          path={data.coins[0].item.small}
+          name={data.coins[0].item.name}
+          symbol={data.coins[0].item.symbol}
+          change={data.coins[0].item.data.price_change_percentage_24h.usd.toFixed(2)}
+        />
+        <TrendingItem
+          path={data.coins[1].item.small}
+          name={data.coins[1].item.name}
+          symbol={data.coins[1].item.symbol}
+          change={data.coins[1].item.data.price_change_percentage_24h.usd.toFixed(2)}
+        />
+        <TrendingItem
+          path={data.coins[2].item.small}
+          name={data.coins[2].item.name}
+          symbol={data.coins[2].item.symbol}
+          change={data.coins[2].item.data.price_change_percentage_24h.usd.toFixed(2)}
+        />
       </ul>
     </div>
   );
